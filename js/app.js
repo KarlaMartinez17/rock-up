5a4687bd133330354a26690c5e6f4ec4e6978ce6
KarlaMartinez17 <karla.martinez_@hotmail.com>
2018-06-24 17:32:59 -0500

    agregando data y modificaciones en general

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
 $(document).ready(function () {
 
-    setTimeout(function() {
-        $('#splash').fadeOut(500); 
-        window.location.href = 'views/principalvw.html';
-     }, 2000);
+    setTimeout(function () {
+        $('#splash').fadeOut(500);
+    }, 2000);
+
+    var inputs = document.getElementsByClassName("formulario-input");
+    for (var i = 0; i < inputs.length; i++) {
+        //evento cuando suelte la tecla cuando termine de escribir
+        inputs[i].addEventListener("keyup", function () {
+            if (this.value.length >= 1) {
+                this.nextElementSibling.classList.add("fijar");
+            } else {
+                this.nextElementSibling.classList.remove("fijar");
+            }
+        });
+    }
+
+
+    $('#submit').click(login);
+
+    function login(event) {
+        event.preventDefault();
+        var email = $('#email-val').val();
+        var password = $('#password-val').val();
+
+        for (var i = 0; i < USERS_DATA.users.length; i++) {
+            var user = USERS_DATA.users[i];
+            if (email === user.email && password === user.password) {
+                window.location.href = 'views/principalvw.html?userid='+user.id;
+                $('#email-val').val('');
+                $('#password-val').val('');
+            }
+        }
+    }
 });
+
+
+
---
+++ js/data.js
@@ -0,0 +1,45 @@
+var USERS_DATA = {
+    'users': [
+        {
+            'id':1,
+            'name': 'Alejandro Mendoza',
+            'email': 'alexmendoza@gmail.com',
+            'password': '1234567',
+            'photo': 'http://playfm.cl/playfm/site/artic/20160713/imag/foto_0000008420160713150622.jpg'
+        },
+        {
+            'id':2,
+            'name': 'Gustavo Cortes',
+            'email': 'guscor@gmail.com',
+            'password': '1234567',
+            'photo': 'http://playfm.cl/playfm/site/artic/20160713/imag/foto_0000008420160713150622.jpg'
+        },
+        {
+            'id':3,
+            'name': 'Israel Zapata',
+            'email': 'izapata@gmail.com',
+            'password': '1234567',
+            'photo': 'http://stc.obolog.net/multimedia/fotos/12000/11484/11484-12310_s.jpg'
+
+        },
+        {
+            'id':4,
+            'name': 'Karla Morrison',
+            'email': 'KarlaMorr@gmail.com',
+            'password': '1234567',
+            'photo': 'https://i.pinimg.com/originals/6b/8d/5b/6b8d5b47d97840537123563b4ae09ad9.jpg'
+
+        },
+        {
+            'id':5,
+            'name': 'Amy Lee',
+            'email': 'Amylee@gmail.com',
+            'password': '1234567',
+            'photo': 'http://doloritta.com/images/cortes-de-cabello-rockeros-para-mujeres/cortes-de-cabello-rockeros-para-mujeres-56.jpg'
+
+        }
+
+
+    ]
+
+};
