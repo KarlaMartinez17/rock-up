ef03f6df208722048e3d2a171bee440a97f71afc
KarlaMartinez17 <karla.martinez_@hotmail.com>
2018-06-25 13:02:01 -0500

    template user terminado

--- js/principalvw.js
+++ js/principalvw.js
@@ -1,41 +1,41 @@
-function getPostText(event){
+function getPostText(event) {
     event.preventDefault();
     var postText = $('#post-text').val();
-   
-    
+
+
 
     $('#post-text').val('');
-    
+
     var userImage = loggedInUser.photo;
     var userName = loggedInUser.name;
     var numOfLikes = 5;
 
-    var postHtml = '<div class="row">'+
-            '<div class="col-auto foto">'+
-                '<a href="#">'+
-                    '<img src="'+userImage+'" alt="">'+
-                '</a>'+
-            '</div>'+
-            '<div class="col">'+
-                '<div class="post">'+
-                    '<a href="#" class="nombre">'+userName+'</a>'+
-                    '<p class="texto">'+postText+'</p>'+
-                    '<div class="caja-botones d-flex justify-content-between align-items-center">'+
-                        '<button>'+
-                            '<i class="icon-thumbs-up"></i>'+
-                        '</button>'+
-                        '<p>'+numOfLikes+'<i class="icon-thumbs-up"></i></p>'
-                    '</div>'+
-                '</div>'+
-            '</div>'+
-        '</div>'+
-        '<div class="row">'+
-            '<div class="col-10 offset-2">'+
-                '<div class="comentarios"></div>'+
-            '</div>'+
+    var postHtml = '<div class="row">' +
+        '<div class="col-auto foto">' +
+        '<a href="#">' +
+        '<img src="' + userImage + '" alt="">' +
+        '</a>' +
+        '</div>' +
+        '<div class="col">' +
+        '<div class="post">' +
+        '<a href="#" class="nombre">' + userName + '</a>' +
+        '<p class="texto">' + postText + '</p>' +
+        '<div class="caja-botones d-flex justify-content-between align-items-center">' +
+        '<button>' +
+        '<i class="icon-thumbs-up"></i>' +
+        '</button>' +
+        '<p>' + numOfLikes + '<i class="icon-thumbs-up"></i></p>'
+    '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="row">' +
+        '<div class="col-10 offset-2">' +
+        '<div class="comentarios"></div>' +
+        '</div>' +
         '</div>';
 
-    
+
     var firstPost = $('#publicaciones .publicacion')[0];
 
     var post = document.createElement('div');
@@ -43,21 +43,21 @@ function getPostText(event){
     post.innerHTML = postHtml;
 
     $(post).insertBefore(firstPost);
-    
+
 }
 
-var loggedInUser ;
+var loggedInUser;
 
-$(document).ready(function (){
+$(document).ready(function () {
     $('#publish').click(getPostText);
     console.log(localStorage.rockUpUserId);
 
     for (var i = 0; i < USERS_DATA.users.length; i++) {
         var user = USERS_DATA.users[i];
-        
-        if( user.id === parseInt(localStorage.rockUpUserId) ) {
-            loggedInUser= user;
-            $('.ingreso-perfil').each(function(){
+
+        if (user.id === parseInt(localStorage.rockUpUserId)) {
+            loggedInUser = user;
+            $('.ingreso-perfil').each(function () {
                 $(this).attr('src', loggedInUser.photo);
                 $(this).attr('user', loggedInUser.id);
             });
@@ -65,12 +65,12 @@ $(document).ready(function (){
     }
     $('.abrir-perfil').click(openProfile);
 
-    function openProfile(event){
+    function openProfile(event) {
         event.preventDefault();
         console.log('hola');
         var userId = $(this).attr('user');
         console.log(userId);
-        localStorage.rockUpProfileId = userId;  
+        localStorage.rockUpProfileId = userId;
         window.location.href = 'user.html';
 
     }
--- js/user.js
+++ js/user.js
@@ -1,14 +1,14 @@
-$(document).ready(function (){
+$(document).ready(function () {
     var userProfile;
     for (var i = 0; i < USERS_DATA.users.length; i++) {
         var user = USERS_DATA.users[i];
-        
-        if( user.id === parseInt(localStorage.rockUpProfileId) ) {
-            userProfile= user;
-            
+
+        if (user.id === parseInt(localStorage.rockUpProfileId)) {
+            userProfile = user;
+
             $('#profile').attr('src', userProfile.photo);
             $('#profile-name').html(userProfile.name);
-            $('#portada').css('background-image', 'url('+userProfile.cover+')');  
+            $('#portada').css('background-image', 'url(' + userProfile.cover + ')');
         }
     }
 });
